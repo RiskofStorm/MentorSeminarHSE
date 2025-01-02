f494246e524fa274c698b4ae1c37ae3797a6b0c1
riskofstorm <riskofstorm@gmail.com>
2025-01-02 15:53:17 +0300

    updated shorturl_app

--- shorturl_app/main.py
+++ shorturl_app/main.py
@@ -3,11 +3,13 @@ from typing import List, Optional
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session, declarative_base
 from sqlalchemy import  Column, Integer, String, Boolean
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.responses import RedirectResponse
+import string
+import random
 
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/shorturl_app.data"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/shorturl_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -16,13 +18,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 Base = declarative_base()
-# class TodoTable(Base):
-#     __tablename__ = 'todo_notes'
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     title = Column(String, index=True)
-#     description = Column(String, nullable=True)
-#     completed = Column(Boolean, nullable=False)
+class URLItem(Base):
+    __tablename__ = 'short_urls'
+
+    id = Column(Integer, primary_key=True, index=True)
+    short_id = Column(String, unique=True, index=True)
+    full_url = Column(String)
 
 # создаем таблицу
 Base.metadata.create_all(bind=engine)
@@ -37,95 +38,58 @@ def _start_session() -> Session:
         session.close()
 
 # DATA VALIDATION
-# class TodoInsert(BaseModel):
-#     title: str
-#     description: Optional[str] = None
-#     completed: bool = False
-#
-# class TodoTableDQ(BaseModel):
-#     id: int
-#     title: str
-#     description: Optional[str] = None
-#     completed: bool = False
-#
-#     class Config:
-#         from_attributes = True
+class URLCreate(BaseModel):
+    url: HttpUrl
+
+def gen_id(id_len=7):
+    alphabet = string.ascii_lowercase + string.digits
+    return ''.join(random.choices(alphabet, k=id_len))
+
 
 #-------------------
 # APP METHODS
 #-------------------
 
-# # POST
-# @app.post("/items", response_model=TodoTableDQ)
-# def insert_item(item: TodoInsert,
-#                 session: Session = Depends(_start_session)):
-#     row = TodoTable(
-#         title=item.title,
-#         description=item.description,
-#         completed=item.completed
-#     )
-#     session.add(row)
-#     session.commit()
-#     session.refresh(row)
-#     return row
-#
-# # GET all items
-# @app.get("/items", response_model=List[TodoTableDQ])
-# def get_items(session: Session = Depends(_start_session)):
-#     return session.query(TodoTable).all()
-#
-# #GET items by id
-# @app.get("/items/{item_id}", response_model=TodoTableDQ)
-# def get_items_by_id(id: int,
-#                     session: Session = Depends(_start_session)):
-#     row = (session.query(TodoTable)
-#                   .filter(TodoTable.id == id)
-#                   .first()
-#     )
-#     if row:
-#         return row
-#     raise HTTPException(status_code=404, detail=f"Item id = {id} not found")
-#
-#
-# #PUT items by id
-# @app.put("/items/{item_id}", response_model=TodoTableDQ)
-# def get_items_by_id(id: int,
-#                     session: Session = Depends(_start_session)):
-#     ret_val = (session.query(TodoTable)
-#                   .filter(TodoTable.id == id)
-#                   .first()
-#     )
-#     if ret_val:
-#         ret_val.title = TodoTable.title
-#         ret_val.description = TodoTable.description
-#         ret_val.completed = TodoTable.completed
-#         session.commit()
-#         session.refresh(ret_val)
-#         return ret_val
-#     raise HTTPException(status_code=404, detail=f"Item id = {id} not found")
-#
-#
-# #DELETE all rows from "todo_notes" table
-# @app.delete("/items/{item_id}")
-# def delete_item(id: int,
-#                 session: Session = Depends(_start_session)):
-#     ret_val = (session.query(TodoTable)
-#                   .filter(TodoTable.id == id)
-#                   .first()
-#     )
-#     if ret_val:
-#         session.delete(ret_val)
-#         session.commit()
-#         return {"message": f"item with id {id} was deleted from database successfully"}
-#     raise HTTPException(status_code=404, detail=f"Item id = {id} not found")
-#
-# @app.delete("/items")
-# def delete_db(session: Session = Depends(_start_session)):
-#     import shutil
-#     import datetime
-#     timestamp  = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-#     shutil.copy2('./data/todo_app.data',f'./data/todo_app_{timestamp}.data')
-#
-#     session.query(TodoTable).delete()
-#     session.commit()
-#     return {"message": "All data from app was deleted"}
+@app.post("/shorten")
+def post_shorten_url(item: URLCreate,
+                sess: Session = Depends(_start_session)):
+    for _ in range(20):
+        short_id = gen_id()
+        try:
+            new_link = URLItem(short_id=short_id, full_url=str(item.url))
+            sess.add(new_link)
+            sess.commit()
+            sess.refresh(new_link)
+            return {'short_url': f'http://localhost:8000/{short_id}'}
+        except:
+            raise HTTPException(status_code=500, detail="Something went wrong, please try again")
+
+@app.get("/{short_id}")
+def get_full_url(short_id: str,
+                     sess: Session = Depends(_start_session)):
+    url_item = (
+        sess.query(URLItem)
+            .filter(URLItem.short_id == short_id)
+            .first()
+    )
+    if url_item:
+        return RedirectResponse(url=str(url_item.full_url), status_code=302)
+    raise HTTPException(status_code=404, detail="Short url not found")
+
+
+
+@app.get("/stats/{short_id}")
+def get_stats(short_id: str,
+              sess: Session = Depends(_start_session)):
+    url_item = (
+        sess.query(URLItem)
+            .filter(URLItem.short_id == short_id)
+            .first()
+    )
+    if url_item:
+        return {
+            'short_id': url_item.short_id,
+            'full_url': url_item.full_url
+        }
+    raise HTTPException(status_code=404, detail="Short url not found")
+
